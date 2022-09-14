27e77dd4541ee80d03307a6b77f733b616ad0876
Diogo <cfeld.diogo@gmail.com>
2022-09-14 00:22:46 -0300

    delete

--- GigaVistor/GigaVistor/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GigaVistor.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
