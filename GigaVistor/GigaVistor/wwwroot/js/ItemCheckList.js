98df7ee8155eb61408fa28dac62bda81627df59c
Diogo <cfeld.diogo@gmail.com>
2022-10-30 14:54:15 -0300

    begging of CheckList Itens

---
+++ GigaVistor/GigaVistor/wwwroot/js/ItemCheckList.js
@@ -0,0 +1,41 @@
+﻿function addItemToList() {
+    let checklistTable = document.getElementById("checklistTable");
+
+    let newRow = document.createElement("tr");
+    checklistTable.appendChild(newRow);
+
+    let descriptionTd = document.createElement("td");
+    newRow.appendChild(descriptionTd);
+
+
+    let responsavelTd = document.createElement("td");
+    newRow.appendChild(responsavelTd);
+
+    let aderenteTd = document.createElement("td");
+    newRow.appendChild(aderenteTd);
+
+    let statusTd = document.createElement("td");
+    newRow.appendChild(statusTd);
+
+    let prazoTd = document.createElement("td");
+    newRow.appendChild(prazoTd);
+
+    let NCTd = document.createElement("td");
+    newRow.appendChild(NCTd);
+
+    let seriedadeNCTd = document.createElement("td");
+    newRow.appendChild(seriedadeNCTd);
+
+    let descricaoNCTd = document.createElement("td");
+    newRow.appendChild(descricaoNCTd);
+
+    let statusPosNCTd = document.createElement("td");
+    newRow.appendChild(statusPosNCTd);
+
+    let prazoNCTd = document.createElement("td");
+    newRow.appendChild(prazoNCTd);
+
+    let buttonAddNCTd = document.createElement("td");
+    newRow.appendChild(buttonAddNCTd);
+
+}
